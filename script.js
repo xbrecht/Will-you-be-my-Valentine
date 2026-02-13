fc06e8d293aef8dee75cc4306375795634f5d5e4
xbrecht <77734489+xbrecht@users.noreply.github.com>
2026-02-13 17:04:12 +0100

    Update script.js

--- script.js
+++ script.js
@@ -54,16 +54,16 @@
 })();
 */
 const messages = [
-    "Are you sure?",
-    "Really sure??",
-    "Are you positive?",
-    "Pookie please...",
-    "Just think about it!",
-    "If you say no, I will be really sad...",
-    "I will be very sad...",
-    "I will be very very very sad...",
-    "Ok fine, I will stop asking...",
-    "Just kidding, say yes please! ❤️"
+    "Sicher?",
+    "Ernsthaaaft??",
+    "Wow... das hätt ich mich nicht getraut.",
+    "Schaaaaaaatz... :o", 
+    "Denk nochmal nach!",
+    "Du würdest mich traurig machen...",
+    "Felix traurig...",
+    "Dann wäre ich seeeehr traurig...",
+    "Ok dann frag ich dich halt nicht...",
+    "Spaß! Bittteeeeeeeee ❤️"
 ];
 
 let messageIndex = 0;
